e628b40264fa5f2e367c57a116e97fb8572620c8
Quantum Snowball <quantum.snowball@gmail.com>
2024-11-02 15:15:41 +0800

    xiyaowong/nvim-transparent

--- lua/plugins/nvim-transparent.lua
+++ lua/plugins/nvim-transparent.lua
@@ -3,25 +3,26 @@
 return {
     'xiyaowong/nvim-transparent',
     event = 'VimEnter',
-    config = function()
-        require("transparent").setup({
-            extra_groups = { -- table/string: additional groups that should be cleared
-                -- In particular, when you set it to 'all', that means all available groups
-
-                -- example of akinsho/nvim-bufferline.lua
-                "BufferLineTabClose",
-                "BufferlineBufferSelected",
-                "BufferLineFill",
-                "BufferLineBackground",
-                "BufferLineSeparator",
-                "BufferLineIndicatorSelected",
-            },
-            exclude_groups = {
-                "MatchParen",
-                "CursorLine",
-                "CursorColumn",
-            }, -- table: groups you don't want to clear
-        })
+    opts = {
+        -- table/string: additional groups that should be cleared
+        extra_groups = {
+            -- In particular, when you set it to 'all', that means all available groups
+            -- example of akinsho/nvim-bufferline.lua
+            "BufferLineTabClose",
+            "BufferlineBufferSelected",
+            "BufferLineFill",
+            "BufferLineBackground",
+            "BufferLineSeparator",
+            "BufferLineIndicatorSelected",
+        },
+        -- table: groups you don't want to clear
+        exclude_groups = {
+            "MatchParen",
+            "CursorLine",
+            "CursorColumn",
+        },
+    },
+    init = function()
         -- custom key bindings
         local map = require('utils').map
         -- avaiable commands:
