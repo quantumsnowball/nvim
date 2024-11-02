37e866402950f9feb4c5c15a2fbc8d03bf305176
Quantum Snowball <quantum.snowball@gmail.com>
2024-11-02 15:27:46 +0800

    s1n7ax/nvim-window-picker

--- lua/plugins/neo-tree-nvim.lua
+++ lua/plugins/neo-tree-nvim.lua
@@ -13,25 +13,25 @@ return {
         event = 'VeryLazy',
         keys = {
             -- float
-            { '<leader>e', '<cmd>Neotree reveal float toggle<cr>', desc = 'Neotree Floating' },
-            { '<space>e', '<cmd>Neotree reveal float toggle<cr>', desc = 'Neotree Floating' },
-            { 'qq', '<cmd>Neotree reveal float toggle<cr>', desc = 'Neotree Floating' },
-            { 'qe', '<cmd>Neotree reveal float toggle<cr>', desc = 'Neotree Floating' },
+            { '<leader>e',  '<cmd>Neotree reveal float toggle<cr>',     desc = 'Neotree Floating' },
+            { '<space>e',   '<cmd>Neotree reveal float toggle<cr>',     desc = 'Neotree Floating' },
+            { 'qq',         '<cmd>Neotree reveal float toggle<cr>',     desc = 'Neotree Floating' },
+            { 'qe',         '<cmd>Neotree reveal float toggle<cr>',     desc = 'Neotree Floating' },
             -- left
-            { '<leader>EH', '<cmd>Neotree reveal left toggle<cr>', desc = 'Neotree Left' },
-            { '<space>EH', '<cmd>Neotree reveal left toggle<cr>', desc = 'Neotree Left' },
+            { '<leader>EH', '<cmd>Neotree reveal left toggle<cr>',      desc = 'Neotree Left' },
+            { '<space>EH',  '<cmd>Neotree reveal left toggle<cr>',      desc = 'Neotree Left' },
             -- right
-            { '<leader>EL', '<cmd>Neotree reveal right toggle<cr>', desc = 'Neotree Right' },
-            { '<space>EL', '<cmd>Neotree reveal right toggle<cr>', desc = 'Neotree Right' },
+            { '<leader>EL', '<cmd>Neotree reveal right toggle<cr>',     desc = 'Neotree Right' },
+            { '<space>EL',  '<cmd>Neotree reveal right toggle<cr>',     desc = 'Neotree Right' },
             -- top
-            { '<leader>EK', '<cmd>Neotree reveal top toggle<cr>', desc = 'Neotree Top' },
-            { '<space>EK', '<cmd>Neotree reveal top toggle<cr>', desc = 'Neotree Top' },
+            { '<leader>EK', '<cmd>Neotree reveal top toggle<cr>',       desc = 'Neotree Top' },
+            { '<space>EK',  '<cmd>Neotree reveal top toggle<cr>',       desc = 'Neotree Top' },
             -- bottom
-            { '<leader>EJ', '<cmd>Neotree reveal bottom toggle<cr>', desc = 'Neotree Bottom' },
-            { '<space>EJ', '<cmd>Neotree reveal bottom toggle<cr>', desc = 'Neotree Bottom' },
+            { '<leader>EJ', '<cmd>Neotree reveal bottom toggle<cr>',    desc = 'Neotree Bottom' },
+            { '<space>EJ',  '<cmd>Neotree reveal bottom toggle<cr>',    desc = 'Neotree Bottom' },
             -- git
             { '<leader>EG', '<cmd>Neotree git_status float toggle<cr>', desc = 'Neotree Git Status' },
-            { '<space>EG', '<cmd>Neotree git_status float toggle<cr>', desc = 'Neotree Git Status' },
+            { '<space>EG',  '<cmd>Neotree git_status float toggle<cr>', desc = 'Neotree Git Status' },
         },
         opts = {
             sources = {
@@ -98,24 +98,22 @@ return {
         's1n7ax/nvim-window-picker',
         -- tag = 'v1.*',
         lazy = true,
-        config = function()
-            require 'window-picker'.setup({
-                autoselect_one = true,
-                include_current = false,
-                selection_chars = '123456789ABCDEF',
-                use_winbar = 'always',
-                filter_rules = {
-                    -- filter using buffer options
-                    bo = {
-                        -- if the file type is one of following, the window will be ignored
-                        filetype = { 'neo-tree', "neo-tree-popup", "notify" },
+        opts = {
+            autoselect_one = true,
+            include_current = false,
+            selection_chars = '123456789ABCDEF',
+            use_winbar = 'always',
+            filter_rules = {
+                -- filter using buffer options
+                bo = {
+                    -- if the file type is one of following, the window will be ignored
+                    filetype = { 'neo-tree', "neo-tree-popup", "notify" },
 
-                        -- if the buffer type is one of following, the window will be ignored
-                        buftype = { 'terminal', "quickfix" },
-                    },
+                    -- if the buffer type is one of following, the window will be ignored
+                    buftype = { 'terminal', "quickfix" },
                 },
-                -- other_win_hl_color = '#e35e4f',
-            })
-        end,
+            },
+            -- other_win_hl_color = '#e35e4f',
+        }
     }
 }
