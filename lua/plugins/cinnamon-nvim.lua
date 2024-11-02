708a22f83e32678402fadbfc43281f652d38a1d1
Quantum Snowball <quantum.snowball@gmail.com>
2024-11-02 15:38:53 +0800

    declancm/cinnamon.nvim

--- lua/plugins/cinnamon-nvim.lua
+++ lua/plugins/cinnamon-nvim.lua
@@ -3,9 +3,8 @@
 return {
     'declancm/cinnamon.nvim',
     event = 'VeryLazy',
-    config = function()
-        require('cinnamon').setup {
-            delay = 0, -- The default delay (in ms) between each line when scrolling.
-        }
-    end
+    -- TODO: need to define keymaps to require("cinnamon").scroll("<keymaps>") to use animations
+    opts = {
+        delay = 5, -- The default delay (in ms) between each line when scrolling.
+    },
 }
