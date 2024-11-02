5537c31b686207fe95d7462a7074467ca01c5f5a
Quantum Snowball <quantum.snowball@gmail.com>
2024-11-02 14:46:30 +0800

    lazygit

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -7,7 +7,7 @@ return {
         { '<leader>gg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
         { '<leader>c', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
     },
-    config = function()
+    init = function()
         -- config
         -- vim.g.lazygit_floating_window_winblend = 30
         vim.g.lazygit_floating_window_scaling_factor = 1.0
