927486c65800fa99edd286d33f3f3023e9162d00
Quantum Snowball <quantum.snowball@gmail.com>
2024-11-02 23:16:34 +0800

    <leader>sr, map

--- lua/plugins/nvim-spectre.lua
+++ lua/plugins/nvim-spectre.lua
@@ -13,16 +13,25 @@ return {
         }
     },
     init = function()
+        local map = require('utils').map
         -- open_visual is delayed by one if setting up using keys props
         -- temp fix: setup keymaps using vim cmd
         --
         -- step: open, pull to far bottom, enter insert
-        vim.cmd "nnoremap <silent> <leader>sr :lua require('spectre').open()<CR><c-w>Ji"
-        -- step: copy, restore selection, open_visual, pull to far right, enter insert, paste from " register
-        vim.cmd "nnoremap <silent> <leader>SR :lua require('spectre').open()<CR><c-w>Li"
+        map('n', '<leader>sr',
+            ":lua require('spectre').open()<CR><c-w>Ji",
+            { noremap = true, desc = 'spectre: search and replace, bottom window' })
+        -- step: open, pull to far right, enter insert
+        map('n', '<leader>SR',
+            ":lua require('spectre').open()<CR><c-w>Li",
+            { noremap = true, desc = 'spectre: search and replace, right window' })
         -- step: copy, restore selection, open_visual, pull to far bottom, enter insert, paste from " register
-        vim.cmd "vnoremap <silent> <leader>sr ygv<esc>:lua require('spectre').open_visual()<CR><c-w>Jjji<c-r>\""
+        map('v', '<leader>sr',
+            "ygv<esc>:lua require('spectre').open_visual()<CR><c-w>Jjji<c-r>\"",
+            { noremap = true, desc = 'spectre: search and replace selected text, bottom window' })
         -- step: copy, restore selection, open_visual, pull to far right, enter insert, paste from " register
-        vim.cmd "vnoremap <silent> <leader>SR ygv<esc>:lua require('spectre').open_visual()<CR><c-w>Ljji<c-r>\""
+        map('v', '<leader>SR',
+            "ygv<esc>:lua require('spectre').open_visual()<CR><c-w>Ljji<c-r>\"",
+            { noremap = true, desc = 'spectre: search and replace selected text, right window' })
     end
 }
