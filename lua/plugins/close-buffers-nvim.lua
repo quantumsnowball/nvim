ecee03cdf88a7e4d4cb58a25a23bdb7be2452d7b
Quantum Snowball <quantum.snowball@gmail.com>
2024-11-04 16:06:09 +0800

    better define shortcuts

--- lua/plugins/close-buffers-nvim.lua
+++ lua/plugins/close-buffers-nvim.lua
@@ -1,6 +1,6 @@
 -- quickly delete multiple buffers based on the conditions provided
--- https://github.com/kazhala/close-buffers.nvim
 return {
+    -- https://github.com/kazhala/close-buffers.nvim
     'kazhala/close-buffers.nvim',
     event = 'VeryLazy',
     init = function()
@@ -10,7 +10,17 @@ return {
         local close_current_buffer = function()
             delete({ type = 'this' })
         end
-        local close_all_buffers = function()
+        local close_other_buffer = function()
+            delete({ type = 'other' })
+        end
+        local close_other_buffers_and_tabpages = function()
+            vim.cmd('tabonly')
+            delete({ type = 'other' })
+        end
+        local close_other_tabpages = function()
+            vim.cmd('tabonly')
+        end
+        local close_all_buffers_and_tabpages = function()
             vim.cmd('Alpha')
             vim.cmd('tabonly')
             delete({ type = 'other' })
@@ -18,16 +28,12 @@ return {
         local close_hidden_buffers = function()
             delete({ type = 'hidden' })
         end
-        local close_other_tabpages = function()
-            vim.cmd('tabonly')
-            delete({ type = 'hidden' })
-        end
         -- keymaps
-        -- map('n', '<M-w>', close_current_buffer, { desc = 'close current buffer' })
-        -- map('n', '<space>x', close_current_buffer, { desc = 'close current buffer' })
         map('n', 'qb', close_current_buffer, { desc = 'close current buffer' })
-        map('n', 'qa', close_all_buffers, { desc = 'close_all_buffers()' })
-        map('n', 'qh', close_hidden_buffers, { desc = 'close_all_hidden_buffers()' })
-        map('n', 'qo', close_other_tabpages, { desc = 'close_other_tabpages()' })
+        map('n', 'qo', close_other_buffers_and_tabpages, { desc = 'close other buffers and tabpages' })
+        map('n', 'qOb', close_other_buffer, { desc = 'close other buffers' })
+        map('n', 'qOt', close_other_tabpages, { desc = 'close other tabpages' })
+        map('n', 'qa', close_all_buffers_and_tabpages, { desc = 'close all buffers and tabpages' })
+        map('n', 'qh', close_hidden_buffers, { desc = 'close all hidden buffers' })
     end
 }
