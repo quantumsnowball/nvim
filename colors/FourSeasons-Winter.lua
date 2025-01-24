234f18d527f46fc52f406664bc7bf10f82dc75ab
Quantum Snowball <quantum.snowball@gmail.com>
2025-01-25 05:28:43 +0800

    more colors

--- colors/FourSeasons-Winter.lua
+++ colors/FourSeasons-Winter.lua
@@ -5,8 +5,10 @@ local c = colorbuddy.colors
 local g = colorbuddy.groups
 local s = colorbuddy.styles
 
+
 -- setup colorscheme
 colorbuddy.colorscheme("FourSeasons-Winter")
+
 
 -- colors
 C.new('base0', '#191515')
@@ -30,10 +32,42 @@ C.new('yellow', '#f9cc6c')
 C.new('orange', '#f38d70')
 C.new('purple', '#a8a9eb')
 C.new('red', '#fd6883')
+C.new('diff_add', '#527728')
+C.new('diff_remove', '#842335')
+C.new('diff_change', '#247c6e')
+C.new('diff_text', '#23324d')
 
--- Define highlights in terms of `colors` and `groups`
+
+-- groups
+-- common
+G.new('Normal', c.white, c.base2)
+G.new('Identifier', c.white, c.base2)
 G.new('Comment', c.base6, nil)
+G.new('Identifier', c.white, nil)
 G.new('Keyword', c.pink, nil)
+G.new('Include', c.pink, nil)
+G.new('Statement', c.pink, nil)
 G.new('Function', c.green, nil)
 G.new('String', c.yellow, nil)
+G.new('Character', c.yellow, nil)
+G.new('Operator', c.pink, nil)
+G.new('MatchParen', c.pink, nil)
+G.new('Number', c.purple, nil)
+G.new('Float', c.purple, nil)
+G.new('Boolean', c.purple, nil)
+G.new('Type', c.aqua, nil)
+G.new('Constant', c.aqua, nil)
 G.new('Visual', nil, c.base4)
+G.new('Special', c.red, nil)
+G.new('Search', nil, c.yellow)
+G.new('CursorLineNr', c.orange, c.base2)
+G.new('LineNr', c.base5, c.base2)
+G.new('ErrorMsg', c.red, nil)
+G.new('WarningMsg', c.yellow, nil)
+-- Telescope
+G.new('TelescopeBorder', c.base7, nil)
+G.new('TelescopeNormal', c.base8, c.base0)
+G.new('TelescopeSelection', c.white, nil)
+G.new('TelescopeSelectionCaret', c.green, nil)
+G.new('TelescopeMultiSelection', c.pink, nil)
+G.new('TelescopeMatching', c.aqua, nil)
