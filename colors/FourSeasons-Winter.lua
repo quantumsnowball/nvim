46d47f1261514ab3d3d6a4c0e1c5deb25bf7cf32
Quantum Snowball <quantum.snowball@gmail.com>
2025-01-25 04:51:28 +0800

    clear up unwanted

--- colors/FourSeasons-Winter.lua
+++ colors/FourSeasons-Winter.lua
@@ -1,52 +1,39 @@
--- file: colors/my-colorscheme-name.lua
-
 local colorbuddy = require('colorbuddy')
+local C = colorbuddy.Color
+local G = colorbuddy.Group
+local c = colorbuddy.colors
+local g = colorbuddy.groups
+local s = colorbuddy.styles
 
--- Set up your custom colorscheme if you want
+-- setup colorscheme
 colorbuddy.colorscheme("FourSeasons-Winter")
 
--- And then modify as you like
-local Color = colorbuddy.Color
-local colors = colorbuddy.colors
-local Group = colorbuddy.Group
-local groups = colorbuddy.groups
-local styles = colorbuddy.styles
-
--- Use Color.new(<name>, <#rrggbb>) to create new colors
--- They can be accessed through colors.<name>
-Color.new('base0', '#191515')
-Color.new('base1', '#211c1c')
-Color.new('base2', '#2c2525')
-Color.new('base3', '#403838')
-Color.new('base4', '#5b5353')
-Color.new('base5', '#72696a')
-Color.new('base6', '#8c8384')
-Color.new('base7', '#c3b7b8')
-Color.new('base8', '#fff1f3')
-Color.new('border', '#A1B5B1')
-Color.new('brown', '#352e2e')
-Color.new('white', '#fff1f3')
-Color.new('grey', '#72696a')
-Color.new('black', '#000000')
-Color.new('pink', '#FF6188')
-Color.new('green', '#adda78')
-Color.new('aqua', '#85dacc')
-Color.new('yellow', '#f9cc6c')
-Color.new('orange', '#f38d70')
-Color.new('purple', '#a8a9eb')
-Color.new('red', '#fd6883')
+-- colors
+C.new('base0', '#191515')
+C.new('base1', '#211c1c')
+C.new('base2', '#2c2525')
+C.new('base3', '#403838')
+C.new('base4', '#5b5353')
+C.new('base5', '#72696a')
+C.new('base6', '#8c8384')
+C.new('base7', '#c3b7b8')
+C.new('base8', '#fff1f3')
+C.new('border', '#A1B5B1')
+C.new('brown', '#352e2e')
+C.new('white', '#fff1f3')
+C.new('grey', '#72696a')
+C.new('black', '#000000')
+C.new('pink', '#FF6188')
+C.new('green', '#adda78')
+C.new('aqua', '#85dacc')
+C.new('yellow', '#f9cc6c')
+C.new('orange', '#f38d70')
+C.new('purple', '#a8a9eb')
+C.new('red', '#fd6883')
 
 -- Define highlights in terms of `colors` and `groups`
-Group.new('Comment', colors.base6, colors.base0, styles.bold)
-Group.new('Function', colors.green, colors.base0, styles.bold)
-Group.new('String', colors.yellow, colors.base0, styles.bold)
-Group.new('luaFunctionCall', groups.Function, groups.Function, groups.Function)
-
--- Define highlights in relative terms of other colors
--- Group.new('Error', colors.red:light(), nil, styles.bold)
-
--- If you want multiple styles, just add them!
--- Group.new('italicBoldFunction', colors.green, groups.Function, styles.bold + styles.italic)
-
--- If you want the same style as a different group, but without a style: just subtract it!
--- Group.new('boldFunction', colors.yellow, colors.background, groups.italicBoldFunction - styles.italic)
+G.new('Comment', c.base6, nil)
+G.new('Keyword', c.pink, nil)
+G.new('Function', c.green, nil)
+G.new('String', c.yellow, nil)
+G.new('Visual', nil, c.base4)
