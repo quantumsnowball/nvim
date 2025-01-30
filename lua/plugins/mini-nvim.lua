283c4338bfa6d933d169112305db3c693fd22302
Quantum Snowball <quantum.snowball@gmail.com>
2025-01-31 03:44:47 +0800

    mini.splitjoin

--- lua/plugins/mini-nvim.lua
+++ lua/plugins/mini-nvim.lua
@@ -7,5 +7,15 @@ return {
         require('mini.ai').setup({
             --
         })
+
+        require('mini.splitjoin').setup({
+            -- Module mappings. Use `''` (empty string) to disable one.
+            -- Created for both Normal and Visual modes.
+            mappings = {
+                toggle = 'gs',
+                split = '',
+                join = '',
+            },
+        })
     end
 }
