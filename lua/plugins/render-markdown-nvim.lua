f823a5b4d4f123998c492f3cfaecf5515812e5f3
Quantum Snowball <quantum.snowball@gmail.com>
2025-01-31 01:41:11 +0800

    render-markdown-nvim.lua

---
+++ lua/plugins/render-markdown-nvim.lua
@@ -0,0 +1,11 @@
+-- render-markdown.nvim
+-- https://github.com/MeanderingProgrammer/render-markdown.nvim
+return {
+    'MeanderingProgrammer/render-markdown.nvim',
+    ft = { "markdown" },
+    opts = {
+        file_types = {
+            "markdown"
+        },
+    },
+}
