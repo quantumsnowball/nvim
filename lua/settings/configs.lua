8a900c63b794f4f28f6b73029d864c0ca0c09445
Quantum Snowball <quantum.snowball@gmail.com>
2025-01-31 05:15:33 +0800

    better tabpage tab color

--- lua/settings/configs.lua
+++ lua/settings/configs.lua
@@ -37,5 +37,7 @@ vim.cmd('autocmd VimEnter,BufEnter,ColorScheme * hi SpellLocal guibg=#770000')
 vim.cmd('autocmd VimEnter,ColorScheme * hi BufferLineTab guifg=White guibg=#226622')
 vim.cmd('autocmd VimEnter,ColorScheme * hi BufferLineTabSelected guifg=Black guibg=#22ff44')
 vim.cmd('autocmd VimEnter,ColorScheme * hi BufferLineTabClose guifg=White')
+vim.cmd('autocmd VimEnter,ColorScheme * hi BufferLineTabSeparator guifg=White guibg=#226622')
+vim.cmd('autocmd VimEnter,ColorScheme * hi BufferLineTabSeparatorSelected guifg=White guibg=#22ff44')
 -- list help buffer
 vim.cmd('autocmd FileType help set buflisted')
