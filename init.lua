9f3baf0b4b2b141a82d47ad74e146a49f4e01345
Quantum Snowball <quantum.snowball@gmail.com>
2025-02-01 16:39:17 +0800

    use more official and detailed config table

--- init.lua
+++ init.lua
@@ -19,8 +19,34 @@ vim.opt.termguicolors = true
 vim.g.mapleader = ','
 
 
--- lazy.nvim setup
-require("lazy").setup("plugins")
+-- Setup lazy.nvim
+require("lazy").setup({
+    -- ref:
+    -- https://github.com/folke/lazy.nvim/blob/main/lua/lazy/core/config.lua
+    spec = {
+        -- import your plugins
+        { import = "plugins" },
+    },
+    defaults = {
+        -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
+        -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
+        lazy = false,
+        -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+        -- have outdated releases, which may break your Neovim install.
+        version = false, -- always use the latest git commit
+        -- version = "*", -- try installing the latest stable version for plugins that support semver
+    },
+    -- Configure any other settings here. See the documentation for more details.
+    -- colorscheme that will be used when installing plugins.
+    install = {
+        colorscheme = { "habamax" }
+    },
+    -- automatically check for plugin updates
+    checker = {
+        enabled = true,
+        notify = true,
+    },
+})
 
 
 -- other nvim settings
