a054db40990c5304e948ab3cc75b2bb122f681db
Quantum Snowball <quantum.snowball@gmail.com>
2025-02-11 00:27:03 +0800

    cmp-cmdline completion added

--- lua/plugins/nvim-cmp/init.lua
+++ lua/plugins/nvim-cmp/init.lua
@@ -3,16 +3,34 @@
 return {
     "hrsh7th/nvim-cmp",
     version = false, -- last release is way too old
-    event = "InsertEnter",
+    event = "VeryLazy",
     dependencies = {
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-buffer",
         "hrsh7th/cmp-path",
+        'hrsh7th/cmp-cmdline',
         'L3MON4D3/LuaSnip',
     },
-    opts = function()
+    config = function()
         local cmp = require("cmp")
-        return {
+
+        -- cmdline cmp
+        cmp.setup.cmdline(':', {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = cmp.config.sources({
+                { name = 'path' }
+            }, {
+                {
+                    name = 'cmdline',
+                    option = {
+                        ignore_cmds = { 'Man', '!' }
+                    }
+                }
+            })
+        })
+
+        -- buffer cmp
+        cmp.setup {
             snippet = {
                 expand = function(args)
                     require('luasnip').lsp_expand(args.body)
