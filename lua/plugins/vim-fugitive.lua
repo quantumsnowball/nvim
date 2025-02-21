c6309a6090c6e6e7961a087afa0f363814011ffc
Quantum Snowball <quantum.snowball@gmail.com>
2025-02-21 20:33:48 +0800

    ,a = git commit --amend

--- lua/plugins/vim-fugitive.lua
+++ lua/plugins/vim-fugitive.lua
@@ -4,8 +4,8 @@ return {
     'tpope/vim-fugitive',
     event = 'VeryLazy',
     keys = {
-        { '<leader>c', ':Git commit<cr>', desc = 'Git commit', silent = true },
-        { '<leader>P', ':Git push<cr>',   desc = 'Git push',   silent = true },
+        { '<leader>c', ':Git commit<cr>',         desc = 'Git commit',         silent = true },
+        { '<leader>a', ':Git commit --amend<cr>', desc = 'Git commit --amend', silent = true },
         { '<leader>P', ':!git push &<cr>',        desc = 'Git push',           silent = true },
     },
 }
