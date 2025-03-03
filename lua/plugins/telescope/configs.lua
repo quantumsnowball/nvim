c68fe2d205b6e81a16d7a493502aa2745fe66717
Quantum Snowball <quantum.snowball@gmail.com>
2025-03-03 14:56:09 +0800

    telescope open tabnew and splits shortcuts

--- lua/plugins/telescope/configs.lua
+++ lua/plugins/telescope/configs.lua
@@ -15,6 +15,9 @@ M.defaults = {
             p = require('telescope.actions.layout').toggle_preview,
             J = require('telescope.actions').cycle_history_next,
             K = require('telescope.actions').cycle_history_prev,
+            t = require('telescope.actions').select_tab,
+            ["\\"] = require('telescope.actions').select_vertical,
+            ["-"] = require('telescope.actions').select_horizontal,
         },
         i = {
             ["<C-j>"] = require('telescope.actions').cycle_history_next,
@@ -64,10 +67,10 @@ M.pickers = {
 
 M.extensions = {
     fzf = {
-        fuzzy = true, -- false will only do exact matching
+        fuzzy = true,                   -- false will only do exact matching
         override_generic_sorter = true, -- override the generic sorter
-        override_file_sorter = true, -- override the file sorter
-        case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+        override_file_sorter = true,    -- override the file sorter
+        case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
         -- the default case_mode is "smart_case"
     },
     ["ui-select"] = {
