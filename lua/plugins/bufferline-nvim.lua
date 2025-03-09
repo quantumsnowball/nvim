2b9807114025c8e3b2a0e4f29f68f36d3902b362
Quantum Snowball <quantum.snowball@gmail.com>
2025-03-10 01:02:05 +0800

    organize code

--- lua/plugins/bufferline-nvim.lua
+++ lua/plugins/bufferline-nvim.lua
@@ -7,6 +7,8 @@ return {
     },
     event = 'VeryLazy',
     opts = function()
+        local opts = {}
+
         local jump_to_buffer_window = function(bufnr)
             local tabnr = vim.fn.tabpagenr()
             local wins = vim.api.nvim_tabpage_list_wins(tabnr)
@@ -19,90 +21,85 @@ return {
             end
         end
 
-        return {
-            options = {
-                -- mode = 'tabs', -- enable to show tabpages mode
-                offsets = { { filetype = "neo-tree", text = "Neotree", text_align = "left" } },
-                show_buffer_close_icons = false,
-                show_close_icon = false,
-                separator_style = "slant", -- "slant" | "slope" | "thick" | "thin"
-                -- sort by buffer's first window position
-                sort_by = function(buffer_a, buffer_b)
-                    -- find first windows id
-                    local winid_a = vim.fn.bufwinid(buffer_a.id)
-                    local winid_b = vim.fn.bufwinid(buffer_b.id)
-                    -- find win coordinates
-                    local winpos_a = vim.api.nvim_win_get_position(winid_a)
-                    local winpos_b = vim.api.nvim_win_get_position(winid_b)
-                    -- sorting
-                    -- sort horizontally windows first
-                    if winpos_a[2] < winpos_b[2] then
+        opts.options = {
+            -- mode = 'tabs', -- enable to show tabpages mode
+            offsets = { { filetype = "neo-tree", text = "Neotree", text_align = "left" } },
+            show_buffer_close_icons = false,
+            show_close_icon = false,
+            separator_style = "slant", -- "slant" | "slope" | "thick" | "thin"
+            -- sort by buffer's first window position
+            sort_by = function(buffer_a, buffer_b)
+                -- find first windows id
+                local winid_a = vim.fn.bufwinid(buffer_a.id)
+                local winid_b = vim.fn.bufwinid(buffer_b.id)
+                -- find win coordinates
+                local winpos_a = vim.api.nvim_win_get_position(winid_a)
+                local winpos_b = vim.api.nvim_win_get_position(winid_b)
+                -- sorting
+                -- sort horizontally windows first
+                if winpos_a[2] < winpos_b[2] then
+                    return true
+                end
+                -- then sort vertical windows
+                if winpos_a[2] == winpos_b[2] and winpos_a[1] < winpos_b[1] then
+                    return true
+                end
+                -- otherwise keep the original order
+                return false
+            end,
+            -- only show active buffer in the current tabpage on bufferline
+            custom_filter = function(cur_buf_num, _)
+                local win_buf_ls = vim.fn.tabpagebuflist(vim.fn.tabpagenr())
+                for _, buf_num in pairs(win_buf_ls) do
+                    if cur_buf_num == buf_num then
                         return true
                     end
-                    -- then sort vertical windows
-                    if winpos_a[2] == winpos_b[2] and winpos_a[1] < winpos_b[1] then
-                        return true
-                    end
-                    -- otherwise keep the original order
-                    return false
-                end,
-                -- only show active buffer in the current tabpage on bufferline
-                custom_filter = function(cur_buf_num, _)
-                    local win_buf_ls = vim.fn.tabpagebuflist(vim.fn.tabpagenr())
-                    for _, buf_num in pairs(win_buf_ls) do
-                        if cur_buf_num == buf_num then
-                            return true
-                        end
-                    end
-                    return false
-                end,
-                -- diagnostics infos
-                diagnostics = 'nvim_lsp',
-                diagnostics_indicator = function(_, _, diagnostics_dict, _)
-                    local s = " "
-                    for e, n in pairs(diagnostics_dict) do
-                        local sym = e == "error" and " "
-                            or (e == "warning" and " " or " ")
-                        s = s .. n .. sym
-                    end
-                    return s
-                end,
-                -- click to focus win containing the buffer
-                left_mouse_command = jump_to_buffer_window,
-                right_mouse_command = jump_to_buffer_window,
-            },
-            highlights = {
-                -- buffer
-                buffer_selected             = {
-                    fg = 'white', bg = '#003366' },
-                separator_selected          = {
-                    fg = '#121212', bg = '#003366',
-                },
-                numbers_selected            = { bg = '#003366' },
-                diagnostic_selected         = { bg = '#003366' },
-                hint_selected               = { bg = '#003366' },
-                hint_diagnostic_selected    = { bg = '#003366' },
-                info_selected               = { bg = '#003366' },
-                info_diagnostic_selected    = { bg = '#003366' },
-                warning_selected            = { bg = '#003366' },
-                warning_diagnostic_selected = { bg = '#003366' },
-                error_selected              = { bg = '#003366' },
-                error_diagnostic_selected   = { bg = '#003366' },
-                modified_selected           = { bg = '#003366' },
-                duplicate_selected          = { bg = '#003366' },
-                indicator_selected          = { bg = '#003366' },
-                pick_selected               = { bg = '#003366' },
-                -- tab
-                tab                         = {
-                    fg = 'white', bg = '#226622' },
-                tab_separator               = {
-                    fg = '#121212', bg = '#226622' },
-                tab_selected                = {
-                    fg = 'black', bg = '#22ff44' },
-                tab_separator_selected      = {
-                    fg = '#121212', bg = '#22ff44' },
-            }
+                end
+                return false
+            end,
+            -- diagnostics infos
+            diagnostics = 'nvim_lsp',
+            diagnostics_indicator = function(_, _, diagnostics_dict, _)
+                local s = " "
+                for e, n in pairs(diagnostics_dict) do
+                    local sym = e == "error" and " "
+                        or (e == "warning" and " " or " ")
+                    s = s .. n .. sym
+                end
+                return s
+            end,
+            -- click to focus win containing the buffer
+            left_mouse_command = jump_to_buffer_window,
+            right_mouse_command = jump_to_buffer_window,
         }
+
+
+        opts.highlights = {
+            -- buffer
+            buffer_selected             = { fg = 'white', bg = '#003366' },
+            separator_selected          = { fg = '#121212', bg = '#003366', },
+            numbers_selected            = { bg = '#003366' },
+            diagnostic_selected         = { bg = '#003366' },
+            hint_selected               = { bg = '#003366' },
+            hint_diagnostic_selected    = { bg = '#003366' },
+            info_selected               = { bg = '#003366' },
+            info_diagnostic_selected    = { bg = '#003366' },
+            warning_selected            = { bg = '#003366' },
+            warning_diagnostic_selected = { bg = '#003366' },
+            error_selected              = { bg = '#003366' },
+            error_diagnostic_selected   = { bg = '#003366' },
+            modified_selected           = { bg = '#003366' },
+            duplicate_selected          = { bg = '#003366' },
+            indicator_selected          = { bg = '#003366' },
+            pick_selected               = { bg = '#003366' },
+            -- tab
+            tab                         = { fg = 'white', bg = '#226622' },
+            tab_separator               = { fg = '#121212', bg = '#226622' },
+            tab_selected                = { fg = 'black', bg = '#22ff44' },
+            tab_separator_selected      = { fg = '#121212', bg = '#22ff44' },
+        }
+
+        return opts
     end,
     init = function()
         vim.opt.termguicolors = true
