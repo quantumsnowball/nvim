88f0d7ca9dd2fe42d133808e34bcbf5842ac8e93
Quantum Snowball <quantum.snowball@gmail.com>
2025-03-09 18:46:16 +0800

    clean up unused

--- lua/plugins/bufferline-nvim.lua
+++ lua/plugins/bufferline-nvim.lua
@@ -15,39 +15,6 @@ return {
             name_formatter = function(buf)
                 return '' .. vim.api.nvim_tabpage_get_number(buf.tabnr)
             end,
-            -- only show active buffer in the current tabpage on bufferline
-            custom_filter = function(cur_buf_num, _)
-                local win_buf_ls = vim.fn.tabpagebuflist(vim.fn.tabpagenr())
-                for _, buf_num in pairs(win_buf_ls) do
-                    if cur_buf_num == buf_num then
-                        return true
-                    end
-                end
-                return false
-            end,
-            -- diagnostics infos
-            diagnostics = 'nvim_lsp',
-            diagnostics_indicator = function(_, _, diagnostics_dict, _)
-                local s = " "
-                for e, n in pairs(diagnostics_dict) do
-                    local sym = e == "error" and " "
-                        or (e == "warning" and " " or " ")
-                    s = s .. n .. sym
-                end
-                return s
-            end,
-            -- click to focus win containing the buffer
-            left_mouse_command = function(bufnr)
-                local tabnr = vim.fn.tabpagenr()
-                local wins = vim.api.nvim_tabpage_list_wins(tabnr)
-                local targetWin = vim.fn.bufwinid(bufnr)
-                for _, win in ipairs(wins) do
-                    if win == targetWin then
-                        vim.api.nvim_set_current_win(targetWin)
-                        return
-                    end
-                end
-            end
         }
     },
     init = function()
