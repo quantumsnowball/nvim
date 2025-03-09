2effafd8da1112cc94bf2ef1fc3ded42fc5bf2b1
Quantum Snowball <quantum.snowball@gmail.com>
2025-03-09 21:35:12 +0800

    sort vertical split first

--- lua/plugins/bufferline-nvim.lua
+++ lua/plugins/bufferline-nvim.lua
@@ -12,32 +12,22 @@ return {
             offsets = { { filetype = "NvimTree", text = "NvimTree", text_align = "left" } },
             show_buffer_close_icons = false,
             show_close_icon = false,
-            -- only show active buffer in the current tabpage on bufferline
+            -- sort by buffer's first window position
             sort_by = function(buffer_a, buffer_b)
-                -- add custom logic
-                -- local modified_a = vim.fn.getftime(buffer_a.path)
-                -- local modified_b = vim.fn.getftime(buffer_b.path)
-                -- return modified_a > modified_b
-                --
-                -- vim.api.nvim_win_get_number
-                --
-                -- print(vim.inspect(buffer_a.id))
+                -- find first windows id
                 local winid_a = vim.fn.bufwinid(buffer_a.id)
                 local winid_b = vim.fn.bufwinid(buffer_b.id)
-                local winnr_a = vim.fn.bufwinnr(buffer_a.id)
-                local winnr_b = vim.fn.bufwinnr(buffer_b.id)
+                -- find win coordinates
                 local winpos_a = vim.api.nvim_win_get_position(winid_a)
                 local winpos_b = vim.api.nvim_win_get_position(winid_b)
-                local dist_a = winpos_a[1] * winpos_a[2]
-                local dist_b = winpos_b[1] * winpos_b[2]
-                -- print(vim.inspect(winpos_a))
-                -- print('winid: ' .. winid_a .. ', ' .. winid_b)
-                -- print('winnr: ' .. winnr_a .. ', ' .. winnr_b)
-                -- print(winid_a .. ', ' .. winid_b)
-
-                -- return winpos_a[2] > winpos_b[2]
+                -- distance score from top left corner
+                -- amplifying column index ensuring vertical splits sorted earlier
+                local dist_a = winpos_a[1] * winpos_a[2] * 10
+                local dist_b = winpos_b[1] * winpos_b[2] * 10
+                -- smaller dist go to top
                 return dist_a < dist_b
             end,
+            -- only show active buffer in the current tabpage on bufferline
             custom_filter = function(cur_buf_num, _)
                 local win_buf_ls = vim.fn.tabpagebuflist(vim.fn.tabpagenr())
                 for _, buf_num in pairs(win_buf_ls) do
