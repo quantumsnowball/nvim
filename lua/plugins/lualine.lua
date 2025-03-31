c9abad208d3d095eb2a487eafd72f700b83f45e3
Quantum Snowball <quantum.snowball@gmail.com>
2025-04-01 05:18:29 +0800

    tidy up info, remove some useless info

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -23,38 +23,23 @@ return {
                     'mode',
                     'selectioncount',
                 },
-                lualine_c = { { 'filename', path = 1 } },
+                lualine_b = {
+                    'branch',
+                    'diff',
+                },
+                lualine_c = {
+                    { 'filename', path = 1 }
+                },
                 lualine_x = {
-                    -- {
-                    --     require("noice").api.status.message.get_hl,
-                    --     cond = require("noice").api.status.message.has,
-                    -- },
-                    {
-                        require("noice").api.status.mode.get,
-                        cond = require("noice").api.status.mode.has,
-                        color = { fg = "#ff9e64" },
-                    },
-                    {
-                        require("noice").api.status.search.get,
-                        cond = require("noice").api.status.search.has,
-                        color = { fg = "#ff9e64" },
-                    },
-                    {
-                        require("noice").api.status.command.get,
-                        cond = require("noice").api.status.command.has,
-                        color = { fg = "#ff9e64" },
-                    }
+                    'searchcount',
+                    'diagnostics',
                 },
                 lualine_y = {
-                    'diagnostics',
                     'lsp_status',
                     'filetype',
-                    'encoding',
-                    'fileformat',
                 },
                 lualine_z = {
                     'location',
-                    'progress',
                 },
             },
             inactive_sections = {
