fc3dceda8559d104a9a02878944164efce1f39d1
Quantum Snowball <quantum.snowball@gmail.com>
2025-04-02 20:46:03 +0800

    outline settings

--- lua/plugins/outline.nvim.lua
+++ lua/plugins/outline.nvim.lua
@@ -6,9 +6,38 @@ return {
     lazy = true,
     cmd = { "Outline", "OutlineOpen" },
     keys = { -- Example mapping to toggle outline
-        { "zo", "<cmd>Outline<CR>", desc = "Toggle outline" },
+        { "<leader>o", "<cmd>belowright Outline<CR>", desc = "Toggle outline" },
+        { "zo",        "<cmd>topleft Outline<CR>",    desc = "Toggle outline" },
     },
     opts = {
-        -- Your setup opts here
+        outline_window = {
+            position = 'left',
+            -- Percentage or integer of columns
+            width = 25,
+            -- (relative_width not working when termianl is resized)
+            relative_width = false,
+            -- press enter jump to location and close
+            auto_close = true,
+            -- this will auto scroll the original buffer
+            auto_jump = true,
+            -- integer milliseconds temporary highlight when jump. better disable for auto jump
+            jump_highlight_duration = false,
+            -- disable to remain in the oiginal buffer, good for outline view only
+            focus_on_open = false,
+        },
+        preview_window = {
+            border = 'rounded',
+            -- Percentage or integer of lines
+            height = 80,
+            relative_height = true,
+            width = 60,
+            relative_width = true,
+            -- use a popup to view the code insteaad of jump
+            auto_preview = false,
+        },
+        symbol_folding = {
+            -- Depth past which nodes will be folded by default. Set to false to unfold all on open.
+            autofold_depth = 5,
+        },
     },
 }
