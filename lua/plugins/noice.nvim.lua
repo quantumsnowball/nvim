ef9320cc6772f1459cf5f1207fb80a904efca025
Quantum Snowball <quantum.snowball@gmail.com>
2025-04-05 19:08:15 +0800

    put notification on top

--- lua/plugins/noice.nvim.lua
+++ lua/plugins/noice.nvim.lua
@@ -91,7 +91,7 @@ return {
             mini = {
                 timeout = 7000,
                 position = {
-                    row = -2,
+                    row = 2,
                 },
                 border = {
                     style = 'none',
