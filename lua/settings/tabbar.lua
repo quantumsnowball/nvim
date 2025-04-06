7b51bef8bb3606ded5aa1587e27c38ad8ae24509
Quantum Snowball <quantum.snowball@gmail.com>
2025-04-06 20:12:40 +0800

    tabbar

--- lua/settings/tabbar.lua
+++ lua/settings/tabbar.lua
@@ -4,15 +4,15 @@
 local map = require('utils').map
 
 -- navigate between buffers
-map('n', 'q-', ':bprev<CR>', { desc = 'previous buffer' })
-map('n', 'q=', ':bnext<CR>', { desc = 'next buffer' })
-map('n', 'q;', ':e #<CR>', { desc = 'edit previous buffer' })
+map('n', 'q-', '<cmd>bprev<CR>', { desc = 'previous buffer' })
+map('n', 'q=', '<cmd>bnext<CR>', { desc = 'next buffer' })
+map('n', 'q;', '<cmd>e #<CR>', { desc = 'edit previous buffer' })
 
 -- navigate between tabpages
-map('n', 'tj', ':tabprevious<cr>', { desc = 'previous tab' })
-map('n', 'tk', ':tabnext<cr>', { desc = 'next tab' })
-map('n', 'th', ':-tabmove<cr>', { desc = 'move tab left' })
-map('n', 'tl', ':+tabmove<cr>', { desc = 'move tab right' })
+map('n', 'tj', '<cmd>tabprevious<cr>', { desc = 'previous tab' })
+map('n', 'tk', '<cmd>tabnext<cr>', { desc = 'next tab' })
+map('n', 'th', '<cmd>-tabmove<cr>', { desc = 'move tab left' })
+map('n', 'tl', '<cmd>+tabmove<cr>', { desc = 'move tab right' })
 map('n', 'tn', function()
     vim.cmd('tabnew')
     vim.cmd('Alpha')
@@ -23,9 +23,9 @@ map('n', 'tN', function()
     vim.cmd('Alpha')
     require('close_buffers').delete({ type = 'nameless' })
 end, { desc = 'open new tab on left' })
-map('n', 'to', ':tab split<cr>:Neotree reveal float<cr>',
+map('n', 'to', '<cmd>tab split<cr><cmd>Neotree reveal float<cr>',
     { desc = 'open current buffer on new tab on right, then open explorer' })
-map('n', 'tO', ':-1tab split<cr>:Neotree reveal float<cr>',
+map('n', 'tO', '<cmd>-1tab split<cr><cmd>Neotree reveal float<cr>',
     { desc = 'open current buffer on new tab on right, then open explorer' })
 map('n', 'tm', '<C-W>T', { desc = 'move window to new tabpage' })
 map('n', '<F12>', '<C-W>T', { desc = 'move window to new tabpage' })
@@ -41,10 +41,10 @@ local close_onto_previous_tab = function()
 end
 map('n', 'qt', close_onto_previous_tab, { desc = 'close tab' })
 map('n', 'tq', close_onto_previous_tab, { desc = 'close tab' })
-map('n', 'tc', ':tabclose<cr>', { desc = 'close tab' })
+map('n', 'tc', '<cmd>tabclose<cr>', { desc = 'close tab' })
 map('n', 't;', 'g<Tab>', { desc = 'previous tab' })
-map('n', 'tH', ':tabfirst<cr>', { desc = 'go to first tab' })
-map('n', 'tL', ':tablast<cr>', { desc = 'go to last tab' })
+map('n', 'tH', '<cmd>tabfirst<cr>', { desc = 'go to first tab' })
+map('n', 'tL', '<cmd>tablast<cr>', { desc = 'go to last tab' })
 
 -- quickly go to a tabpage with number 1 to 9
 for i = 1, 9, 1 do
