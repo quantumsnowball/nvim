a6a6223af044fba1f1c9a4eb9ffaeb32b5c387b5
Quantum Snowball <quantum.snowball@gmail.com>
2025-04-06 20:29:27 +0800

    gitsign

--- lua/plugins/gitsign.lua
+++ lua/plugins/gitsign.lua
@@ -15,10 +15,10 @@ return {
             map('n', '[c', "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", { expr = true })
 
             -- Actions
-            map('n', '<leader>hs', ':Gitsigns stage_hunk<CR>')
-            map('v', '<leader>hs', ':Gitsigns stage_hunk<CR>')
-            map('n', '<leader>hr', ':Gitsigns reset_hunk<CR>')
-            map('v', '<leader>hr', ':Gitsigns reset_hunk<CR>')
+            map('n', '<leader>hs', '<cmd>Gitsigns stage_hunk<CR>')
+            map('v', '<leader>hs', '<cmd>Gitsigns stage_hunk<CR>')
+            map('n', '<leader>hr', '<cmd>Gitsigns reset_hunk<CR>')
+            map('v', '<leader>hr', '<cmd>Gitsigns reset_hunk<CR>')
             map('n', '<leader>hS', '<cmd>Gitsigns stage_buffer<CR>')
             map('n', '<leader>hu', '<cmd>Gitsigns undo_stage_hunk<CR>')
             map('n', '<leader>hR', '<cmd>Gitsigns reset_buffer<CR>')
@@ -30,8 +30,8 @@ return {
             map('n', '<leader>hx', '<cmd>Gitsigns toggle_deleted<CR>')
 
             -- Text object
-            map('o', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
-            map('x', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+            map('o', 'ih', '<cmd><C-U>Gitsigns select_hunk<CR>')
+            map('x', 'ih', '<cmd><C-U>Gitsigns select_hunk<CR>')
         end
     },
     init = function()
