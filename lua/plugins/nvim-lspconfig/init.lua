55f0319675ea5603e0af6e7aa8bfe41beaefb82f
Quantum Snowball <quantum.snowball@gmail.com>
2025-04-10 03:45:41 +0800

    can accept extra opts for individual server

--- lua/plugins/nvim-lspconfig/init.lua
+++ lua/plugins/nvim-lspconfig/init.lua
@@ -40,20 +40,7 @@ return {
             -- ensure_installed = {}
         },
         init = function()
-            -- activate LSP
-            local caps = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
-            local function activate(lspconfig_name, opts)
-                opts = opts or {}
-                opts.capabilities = caps
-                -- lspconfig and mason names are different, mason-lspconfig is the translation
-                local translation = require('mason-lspconfig').get_mappings()
-                local mason_name = translation.lspconfig_to_mason[lspconfig_name]
-                -- ensure server binary is installed before activating it in lspconfig
-                if require('mason-registry').is_installed(mason_name) then
-                    require("lspconfig")[lspconfig_name].setup(opts)
-                end
-            end
-            -- put desiered server here
+            -- desiered server
             local selected_servers = {
                 -- lua
                 'lua_ls',
@@ -68,11 +55,31 @@ return {
                 -- data
                 "jsonls",
             }
+            -- extra server opts
+            local server_opts = {
+                bashls = {
+                    filetypes = { 'sh', 'bash', 'zsh' }
+                },
+            }
+            -- activate LSP
+            local caps = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+            local function activate(lspconfig_name, opts)
+                opts = opts or {}
+                opts.capabilities = caps
+                opts = vim.tbl_deep_extend('force', opts, server_opts[lspconfig_name] or {})
+                -- lspconfig and mason names are different, mason-lspconfig is the translation
+                local translation = require('mason-lspconfig').get_mappings()
+                local mason_name = translation.lspconfig_to_mason[lspconfig_name]
+                -- ensure server binary is installed before activating it in lspconfig
+                if require('mason-registry').is_installed(mason_name) then
+                    require("lspconfig")[lspconfig_name].setup(opts)
+                end
+            end
             for _, name in pairs(selected_servers) do
                 activate(name)
             end
 
-            -- manually install the selected LSPs
+            -- vim cmd to manually install the selected LSPs
             vim.api.nvim_create_user_command('MasonLSPEnsureInstalled', function()
                 local args = table.concat(selected_servers, " ")
                 vim.cmd(":LspInstall " .. args)
