acdfa488d31e0b4195c5ae6da535586476ee6089
Quantum Snowball <quantum.snowball@gmail.com>
2025-04-10 03:10:29 +0800

    nvim-lspconfig will also trigger nvim-cmp to load
    this will prevent some cases lsp has started but completion engine is
    lazyload and no completion autocommand was registered

--- lua/plugins/nvim-lspconfig/init.lua
+++ lua/plugins/nvim-lspconfig/init.lua
@@ -9,6 +9,7 @@ return {
         dependencies = {
             "williamboman/mason.nvim",
             "williamboman/mason-lspconfig.nvim",
+            "hrsh7th/nvim-cmp",
         },
         init = function()
             vim.diagnostic.config({
