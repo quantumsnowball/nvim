8aaa6297be2bf844bc77f3a3654a746072eeaad5
Quantum Snowball <quantum.snowball@gmail.com>
2025-04-19 03:56:38 +0800

    white scope thick bar visible

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -7,25 +7,25 @@ return {
     opts = function()
         -- define highlight groups
         local highlight = {
-            "RainbowRed",
-            "RainbowYellow",
-            "RainbowBlue",
-            "RainbowOrange",
-            "RainbowGreen",
-            "RainbowViolet",
-            "RainbowCyan",
+            'RainbowRed',
+            'RainbowYellow',
+            'RainbowBlue',
+            'RainbowOrange',
+            'RainbowGreen',
+            'RainbowViolet',
+            'RainbowCyan',
         }
         -- create the highlight groups in the highlight setup hook, so they are reset
         -- every time the colorscheme changes
-        local hooks = require("ibl.hooks")
+        local hooks = require('ibl.hooks')
         hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-            vim.api.nvim_set_hl(0, highlight[1], { fg = "#E06C75" })
-            vim.api.nvim_set_hl(0, highlight[2], { fg = "#E5C07B" })
-            vim.api.nvim_set_hl(0, highlight[3], { fg = "#61AFEF" })
-            vim.api.nvim_set_hl(0, highlight[4], { fg = "#D19A66" })
-            vim.api.nvim_set_hl(0, highlight[5], { fg = "#98C379" })
-            vim.api.nvim_set_hl(0, highlight[6], { fg = "#C678DD" })
-            vim.api.nvim_set_hl(0, highlight[7], { fg = "#56B6C2" })
+            vim.api.nvim_set_hl(0, highlight[1], { fg = '#E06C75' })
+            vim.api.nvim_set_hl(0, highlight[2], { fg = '#E5C07B' })
+            vim.api.nvim_set_hl(0, highlight[3], { fg = '#61AFEF' })
+            vim.api.nvim_set_hl(0, highlight[4], { fg = '#D19A66' })
+            vim.api.nvim_set_hl(0, highlight[5], { fg = '#98C379' })
+            vim.api.nvim_set_hl(0, highlight[6], { fg = '#C678DD' })
+            vim.api.nvim_set_hl(0, highlight[7], { fg = '#56B6C2' })
         end)
         -- other opts to send to setup()
         -- - left bar char choice: █ ▉ ▊ ▋ ▌ ▍ ▎ ▏
@@ -37,19 +37,19 @@ return {
             scope = {
                 enabled = true,
                 char = '▊',
-                highlight = "RainbowGreen",
-            }
+                highlight = 'Normal',
+            },
         }
     end,
     init = function()
         -- show space and end-of-line
-        vim.opt.listchars:append("space:⋅")
-        vim.opt.listchars:append("eol:↴")
+        vim.opt.listchars:append('space:⋅')
+        vim.opt.listchars:append('eol:↴')
         -- toggle space and eol chars
         vim.opt.list = false
         require('utils').map('n', '<F6>', '<cmd>set list!<cr>')
         require('utils').map('n', '<space>`', '<cmd>:set list!<cr>')
         -- wrapped lines follow indentation
         vim.opt.breakindent = true
-    end
+    end,
 }
