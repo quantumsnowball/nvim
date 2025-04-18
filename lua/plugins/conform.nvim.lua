f3d915890344edf449f7f0624706c5767e7a5614
Quantum Snowball <quantum.snowball@gmail.com>
2025-04-19 03:55:54 +0800

    put stylua config in .stylua.toml

--- lua/plugins/conform.nvim.lua
+++ lua/plugins/conform.nvim.lua
@@ -1,27 +1,22 @@
 -- format on save, code prettify
 -- https://github.com/stevearc/conform.nvim
 return {
-    "stevearc/conform.nvim",
-    event = "VeryLazy",
+    'stevearc/conform.nvim',
+    event = 'VeryLazy',
     opts = {
         -- enable formatter when useful
         formatters_by_ft = {
-            lua = { "stylua" },
-            python = { "isort", "autopep8" },
-        },
-        formatters = {
-            stylua = {
-                prepend_args = { "--indent-type", "Spaces" },
-            },
+            lua = { 'stylua' },
+            python = { 'isort', 'autopep8' },
         },
     },
     init = function()
-        local map = require("utils").map
-        local format = require("conform").format
+        local map = require('utils').map
+        local format = require('conform').format
 
         -- auto format on save
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            pattern = "*",
+        vim.api.nvim_create_autocmd('BufWritePre', {
+            pattern = '*',
             callback = function(args)
                 -- docs:
                 -- format(opts, callback)
@@ -30,7 +25,7 @@ return {
                     -- as given by official example
                     bufnr = args.buf,
                     -- default skip lsp format, always use a specific formatter
-                    lsp_format = "never",
+                    lsp_format = 'never',
                     -- milliseconds to block for formatting, default = 1000
                     timeout_ms = 500,
                 })
@@ -38,7 +33,7 @@ return {
         })
 
         -- manual trigger keymap
-        map("n", ";f", format, { desc = "conform: format current buffer" })
-        map("v", ";f", format, { desc = "conform: format selected lines" })
+        map('n', ';f', format, { desc = 'conform: format current buffer' })
+        map('v', ';f', format, { desc = 'conform: format selected lines' })
     end,
 }
