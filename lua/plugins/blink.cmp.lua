f6f8827ee927caaad0f199ec6dc25410ec807ef1
Quantum Snowball <quantum.snowball@gmail.com>
2025-04-19 21:48:02 +0800

    custom keymaps

--- lua/plugins/blink.cmp.lua
+++ lua/plugins/blink.cmp.lua
@@ -3,7 +3,9 @@
 return {
     'saghen/blink.cmp',
     -- optional: provides snippets for the snippet source
-    dependencies = { 'rafamadriz/friendly-snippets' },
+    dependencies = {
+        'rafamadriz/friendly-snippets',
+    },
 
     -- use a release tag to download pre-built binaries
     version = '1.*',
@@ -27,7 +29,17 @@ return {
         -- C-k: Toggle signature help (if signature.enabled = true)
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
-        keymap = { preset = 'default' },
+        keymap = {
+            preset = 'none',
+            ['<C-n>'] = { 'show_and_insert', 'select_next', 'fallback' },
+            ['<C-p>'] = { 'show_and_insert', 'select_prev', 'fallback' },
+            ['<Tab>'] = { 'snippet_forward', 'select_and_accept', 'fallback' },
+            ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+            ['<CR>'] = { 'select_and_accept', 'fallback' },
+            ['<C-u>'] = { 'scroll_documentation_up', 'fallback' },
+            ['<C-d>'] = { 'scroll_documentation_down', 'fallback' },
+            ['<Esc>'] = { 'cancel', 'fallback' },
+        },
 
         appearance = {
             -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
@@ -36,12 +48,21 @@ return {
         },
 
         -- (Default) Only show the documentation popup when manually triggered
-        completion = { documentation = { auto_show = false } },
+        completion = {
+            documentation = {
+                auto_show = true,
+            },
+        },
 
         -- Default list of enabled providers defined so that you can extend it
         -- elsewhere in your config, without redefining it, due to `opts_extend`
         sources = {
-            default = { 'lsp', 'path', 'snippets', 'buffer' },
+            default = {
+                'lsp',
+                'snippets',
+                'buffer',
+                'path',
+            },
         },
 
         -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
@@ -49,7 +70,9 @@ return {
         -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
         --
         -- See the fuzzy documentation for more information
-        fuzzy = { implementation = 'prefer_rust_with_warning' },
+        fuzzy = {
+            implementation = 'prefer_rust_with_warning',
+        },
     },
     opts_extend = { 'sources.default' },
 }
