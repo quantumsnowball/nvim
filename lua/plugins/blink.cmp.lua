c39c89e0f48e9897ea6b18853f945ff60a6a5b53
Quantum Snowball <quantum.snowball@gmail.com>
2025-04-21 01:32:00 +0800

    problem solved

--- lua/plugins/blink.cmp.lua
+++ lua/plugins/blink.cmp.lua
@@ -45,11 +45,6 @@ return {
         },
 
         appearance = {
-            -- Sets the fallback highlight groups to nvim-cmp's highlight groups
-            -- Useful for when your theme doesn't support blink.cmp
-            -- Will be removed in a future release
-            -- FIXME: may be need to write a new theme for this purpose?
-            use_nvim_cmp_as_default = false,
             -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
             -- Adjusts spacing to ensure icons are aligned
             nerd_font_variant = 'normal',
