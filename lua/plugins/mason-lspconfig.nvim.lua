417d9db00935c3feae707022cd3507a4ac2dc55f
Quantum Snowball <quantum.snowball@gmail.com>
2025-05-15 20:06:25 +0800

    automatic enable all installed and selected server by calling vim.lsp.enable()

--- lua/plugins/mason-lspconfig.nvim.lua
+++ lua/plugins/mason-lspconfig.nvim.lua
@@ -9,13 +9,8 @@ return {
     opts = {
         -- auto install some servers
         -- ensure_installed = {}
-        -- automatic enable all installed server
-        -- FIXME: since 6b2ba82e, if leave default as true, duplicated lsp server
-        automatic_enable = false,
-    },
-    init = function()
-        -- desiered server
-        local selected_servers = {
+        -- automatic enable all installed and selected server by calling vim.lsp.enable()
+        automatic_enable = {
             -- lua
             'lua_ls',
             -- shell
@@ -28,31 +23,48 @@ return {
             'html',
             -- data
             'jsonls',
-        }
-        -- extra server opts
-        local server_opts = {
-            bashls = {
-                filetypes = { 'sh', 'bash', 'zsh' },
-            },
-        }
-        -- activate each LSP
-        local translation = require('mason-lspconfig').get_mappings()
-        local mason_registry = require('mason-registry')
-        local lspconfig = require('lspconfig')
-        local blink = require('blink.cmp')
-
-        for _, lspconfig_name in pairs(selected_servers) do
-            local opts = {}
-            -- inject capabilities
-            -- https://cmp.saghen.dev/installation.html#lsp-capabilities
-            opts.capabilities = blink.get_lsp_capabilities()
-            -- inject extra server opts
-            opts = vim.tbl_deep_extend('force', opts, server_opts[lspconfig_name] or {})
-            -- lspconfig and mason names are different, mason-lspconfig is the translation
-            local mason_name = translation.lspconfig_to_package[lspconfig_name]
-            -- ensure server binary is installed before activating it in lspconfig
-            if mason_registry.is_installed(mason_name) then lspconfig[lspconfig_name].setup(opts) end
-        end
+        },
+    },
+    init = function()
+        -- -- desiered server
+        -- local selected_servers = {
+        --     -- lua
+        --     'lua_ls',
+        --     -- shell
+        --     'bashls',
+        --     -- python
+        --     'pyright',
+        --     -- web dev
+        --     'ts_ls',
+        --     'cssls',
+        --     'html',
+        --     -- data
+        --     'jsonls',
+        -- }
+        -- -- extra server opts
+        -- local server_opts = {
+        --     bashls = {
+        --         filetypes = { 'sh', 'bash', 'zsh' },
+        --     },
+        -- }
+        -- -- activate each LSP
+        -- local translation = require('mason-lspconfig').get_mappings()
+        -- local mason_registry = require('mason-registry')
+        -- local lspconfig = require('lspconfig')
+        -- local blink = require('blink.cmp')
+        --
+        -- for _, lspconfig_name in pairs(selected_servers) do
+        --     local opts = {}
+        --     -- inject capabilities
+        --     -- https://cmp.saghen.dev/installation.html#lsp-capabilities
+        --     opts.capabilities = blink.get_lsp_capabilities()
+        --     -- inject extra server opts
+        --     opts = vim.tbl_deep_extend('force', opts, server_opts[lspconfig_name] or {})
+        --     -- lspconfig and mason names are different, mason-lspconfig is the translation
+        --     local mason_name = translation.lspconfig_to_package[lspconfig_name]
+        --     -- ensure server binary is installed before activating it in lspconfig
+        --     if mason_registry.is_installed(mason_name) then lspconfig[lspconfig_name].setup(opts) end
+        -- end
 
         -- vim cmd to manually install the selected LSPs
         vim.api.nvim_create_user_command('MasonLSPEnsureInstalled', function()
