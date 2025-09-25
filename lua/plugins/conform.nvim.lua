d89f0a35f8cd0c08e28cb6469cd79db191075f46
Quantum Snowball <quantum.snowball@gmail.com>
2025-09-26 02:01:00 +0800

    formatter for jsonc

--- lua/plugins/conform.nvim.lua
+++ lua/plugins/conform.nvim.lua
@@ -9,6 +9,7 @@ return {
             lua = { 'stylua' },
             python = { 'isort', 'autopep8' },
             json = { 'jq' },
+            jsonc = { 'jq' },
             html = { 'prettier' },
             javascript = { 'prettier' },
             javascriptreact = { 'prettier' },
