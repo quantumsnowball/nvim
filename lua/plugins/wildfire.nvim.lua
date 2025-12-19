2ab7d0f08c94593e3bf28d99fd8be163f73e2cd3
Quantum Snowball <quantum.snowball@gmail.com>
2025-12-20 00:18:53 +0800

    wildfire incremental selection

---
+++ lua/plugins/wildfire.nvim.lua
@@ -0,0 +1,10 @@
+-- smart incremental selection
+-- https://github.com/SUSTech-data/wildfire.nvim
+return {
+    'sustech-data/wildfire.nvim',
+    event = 'VeryLazy',
+    dependencies = {
+        'nvim-treesitter/nvim-treesitter',
+    },
+    opts = {},
+}
