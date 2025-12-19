264c231a8c0758b94637c5c6b4024fa3ae9a0347
Quantum Snowball <quantum.snowball@gmail.com>
2025-12-19 22:51:54 +0800

    this is the minimal config to get python parser to work

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -3,7 +3,21 @@
 --   requires tree-sitter-cli, can be installed via Mason
 return {
     'nvim-treesitter/nvim-treesitter',
-    branch = "main",
+    branch = 'main',
     event = 'VimEnter',
     build = ':TSUpdate',
+    init = function()
+        -- for the new nvim-tresitteer at main branch, need to manually enable treesitter
+        -- via the build vim.treesitter api, nvim-treesitter no longer handle these
+        vim.api.nvim_create_autocmd('FileType', {
+            -- enable the languages here
+            pattern = {
+                'python',
+            },
+            callback = function()
+                local lang = vim.treesitter.language.get_lang(vim.bo.filetype)
+                if lang then vim.treesitter.start() end
+            end,
+        })
+    end,
 }
