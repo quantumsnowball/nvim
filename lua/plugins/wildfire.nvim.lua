921f2805cc1984111b34dd3da63300884ec3b159
Quantum Snowball <quantum.snowball@gmail.com>
2025-12-20 22:27:10 +0800

    smarter init

--- lua/plugins/wildfire.nvim.lua
+++ lua/plugins/wildfire.nvim.lua
@@ -8,9 +8,41 @@ return {
     },
     opts = {
         keymaps = {
-            init_selection = '<Tab>',
+            init_selection = '<leader>is',
             node_incremental = '<Tab>',
             node_decremental = '<BS>',
         },
     },
+    init = function()
+        local map = require('utils').map
+
+        -- smart incremental selection
+        map('n', '<Tab>', function()
+            -- get node
+            local node = vim.treesitter.get_node()
+            if node then
+                -- get node_type
+                local t = node:type()
+                -- special case where I want to select inner word first
+                if
+                    t:find('^string')
+                    or t:find('^class')
+                    or t:find('^function')
+                    or t:find('^function')
+                    or t:find('^if')
+                    or t:find('^elif')
+                    or t:find('^for')
+                    or t:find('^while')
+                    or t:find('^with')
+                    or t:find('^try')
+                    or t:find('^except')
+                then
+                    vim.fn.feedkeys('viw,is')
+                    return
+                end
+            end
+            -- default is init selection
+            vim.fn.feedkeys(',is')
+        end, { desc = 'smart incremental selection' })
+    end,
 }
