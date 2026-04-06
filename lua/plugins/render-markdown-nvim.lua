54931f136fc609984d89d74f895f5a48e9f0d2bf
Quantum Snowball <quantum.snowball@gmail.com>
2026-04-07 06:17:55 +0800

    render codecompanion chat as markdown

--- lua/plugins/render-markdown-nvim.lua
+++ lua/plugins/render-markdown-nvim.lua
@@ -2,10 +2,14 @@
 -- https://github.com/MeanderingProgrammer/render-markdown.nvim
 return {
     'MeanderingProgrammer/render-markdown.nvim',
-    ft = { 'markdown' },
+    ft = {
+        'markdown',
+        'codecompanion'
+    },
     opts = {
         file_types = {
             'markdown',
+            'codecompanion',
         },
     },
 }
