945a26ab19f2eed51a519506caf12e9c0c0d67d7
Quantum Snowball <quantum.snowball@gmail.com>
2026-04-07 09:06:55 +0800

    refactor(lua/plugins/codecompanion.nvim.lua): wrap key mappings in a function
    This commit refactors the key mappings for CodeCompanion into a function to improve modularity and maintainability.

--- lua/plugins/codecompanion.nvim.lua
+++ lua/plugins/codecompanion.nvim.lua
@@ -18,32 +18,34 @@ return {
             },
         },
     },
-    keys = {
-        -- chats
-        { '<leader>ai', ':CodeCompanionChat<cr><c-w>J', desc = 'CodeCompanion Chat', mode = { 'n', 'v' } },
-        { '<leader>a-', ':CodeCompanionChat<cr><c-w>J', desc = 'CodeCompanion Chat', mode = { 'n', 'v' } },
-        { '<leader>a\\', ':CodeCompanionChat<cr>', desc = 'CodeCompanion Chat (V-Split)', mode = { 'n', 'v' } },
-        { '<leader>A', ':CodeCompanionChat<cr><c-w>T', desc = 'CodeCompanion Chat (New Tabpage)', mode = { 'n', 'v' } },
-        -- generate commit message
-        {
-            '<leader>ac',
-            function()
-                vim.cmd('CodeCompanion /commit')
-                vim.cmd('wincmd J')
-                vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-s>", true, false, true))
-            end,
-            desc = 'CodeCompanion Generate Commit Message (H-Split)'
-        },
-        -- explain selection
-        {
-            '<leader>ae',
-            function()
-                vim.cmd('CodeCompanion /explain')
-                vim.cmd('wincmd J')
-                vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-s>", true, false, true))
-            end,
-            desc = 'CodeCompanion Generate Commit Message (H-Split)',
-            mode = 'v'
-        },
-    }
+    keys = function()
+        return {
+            -- chats
+            { '<leader>ai', ':CodeCompanionChat<cr><c-w>J', desc = 'CodeCompanion Chat', mode = { 'n', 'v' } },
+            { '<leader>a-', ':CodeCompanionChat<cr><c-w>J', desc = 'CodeCompanion Chat', mode = { 'n', 'v' } },
+            { '<leader>a\\', ':CodeCompanionChat<cr>', desc = 'CodeCompanion Chat (V-Split)', mode = { 'n', 'v' } },
+            { '<leader>A', ':CodeCompanionChat<cr><c-w>T', desc = 'CodeCompanion Chat (New Tabpage)', mode = { 'n', 'v' } },
+            -- generate commit message
+            {
+                '<leader>ac',
+                function()
+                    vim.cmd('CodeCompanion /commit')
+                    vim.cmd('wincmd J')
+                    vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-s>", true, false, true))
+                end,
+                desc = 'CodeCompanion Generate Commit Message (H-Split)'
+            },
+            -- explain selection
+            {
+                '<leader>ae',
+                function()
+                    vim.cmd('CodeCompanion /explain')
+                    vim.cmd('wincmd J')
+                    vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-s>", true, false, true))
+                end,
+                desc = 'CodeCompanion Generate Commit Message (H-Split)',
+                mode = 'v'
+            },
+        }
+    end
 }
