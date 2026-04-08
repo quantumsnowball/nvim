2dd5a7b8d453c2f2664bed3da103456f11409ad6
Quantum Snowball <quantum.snowball@gmail.com>
2026-04-09 01:14:16 +0800

    default chat layout is horizontal split, simplify key bindings
    - Add default window hotizontal split layout settings for chat mode
    - Remove move_win_bottom helper function and inline functionality
    - Simplify chat key bindings by removing unnecessary window movements
    - Update V-Split chat binding to use horizontal layout

--- lua/plugins/codecompapnion.nvim/init.lua
+++ lua/plugins/codecompapnion.nvim/init.lua
@@ -8,6 +8,8 @@ return {
         "nvim-lua/plenary.nvim",
         "nvim-treesitter/nvim-treesitter",
     },
+    -- default config ref:
+    -- https://github.com/olimorris/codecompanion.nvim/blob/main/lua/codecompanion/config.lua
     opts = {
         opts = {
             -- use TRACE or DEBUG for debug
@@ -20,6 +22,11 @@ return {
             chat = {
                 auto_scroll = false,
                 start_in_insert_mode = true,
+                window = {
+                    layout = "horizontal",
+                    height = 0.5,
+                    position = "bottom",
+                },
             },
         },
         interactions = {
@@ -48,16 +55,15 @@ return {
     },
     keys = function()
         -- helpers
-        local move_win_bottom = function() vim.cmd('wincmd J') end
         local send_prompt = function() vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-s>", true, false, true)) end
 
         return {
             -- action palette
             { '<leader>ap', ':CodeCompanionActions<cr>', desc = 'CodeCompanion Action Palette', mode = { 'n', 'v' } },
             -- chats
-            { '<leader>ai', ':CodeCompanionChat<cr><c-w>J', desc = 'CodeCompanion Chat', mode = { 'n', 'v' } },
-            { '<leader>a-', ':CodeCompanionChat<cr><c-w>J', desc = 'CodeCompanion Chat', mode = { 'n', 'v' } },
-            { '<leader>a\\', ':CodeCompanionChat<cr>', desc = 'CodeCompanion Chat (V-Split)', mode = { 'n', 'v' } },
+            { '<leader>ai', ':CodeCompanionChat<cr>', desc = 'CodeCompanion Chat', mode = { 'n', 'v' } },
+            { '<leader>a-', ':CodeCompanionChat<cr>', desc = 'CodeCompanion Chat', mode = { 'n', 'v' } },
+            { '<leader>a\\', ':CodeCompanionChat<cr><c-w>L', desc = 'CodeCompanion Chat (V-Split)', mode = { 'n', 'v' } },
             { '<leader>A', ':CodeCompanionChat<cr><c-w>T', desc = 'CodeCompanion Chat (New Tabpage)', mode = { 'n', 'v' } },
             -- generate commit message
             {
@@ -72,7 +78,6 @@ return {
                 '<leader>aC',
                 function()
                     vim.cmd('CodeCompanion /ai_commit_chat')
-                    move_win_bottom()
                 end,
                 desc = 'CodeCompanion Generate Commit Message in Chat'
             },
@@ -81,7 +86,6 @@ return {
                 '<leader>ae',
                 function()
                     vim.cmd('CodeCompanion /explain')
-                    move_win_bottom()
                     send_prompt()
                 end,
                 desc = 'CodeCompanion Generate Commit Message (H-Split)',
