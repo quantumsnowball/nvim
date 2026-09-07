317d7c6ee73a04e9cda3141d65662e15cb9ad3aa
Quantum Snowball <quantum.snowball@gmail.com>
2026-09-07 20:05:22 +0800

    on termux also need to manually enable lua_ls like this

--- lua/plugins/mason-lspconfig.nvim.lua
+++ lua/plugins/mason-lspconfig.nvim.lua
@@ -25,8 +25,12 @@ return {
         }
         -- unsupported on termux, need to manually add config here
         local server_opts_termux_specific = {
+            lua_ls = {
+                cmd = { 'lua-language-server', },
+                filetypes = { 'lua', },
+            },
             rust_analyzer = {
-                cmd = { 'rust-analyzer', }, 
+                cmd = { 'rust-analyzer', },
                 filetypes = { 'rust', },
             },
         }
