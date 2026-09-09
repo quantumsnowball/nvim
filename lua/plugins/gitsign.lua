bb484047f9f44ba43a5871e758bde2bc61cf7cc7
Quantum Snowball <quantum.snowball@gmail.com>
2026-09-09 22:51:32 +0800

    toggle show deletion more easy to find

--- lua/plugins/gitsign.lua
+++ lua/plugins/gitsign.lua
@@ -61,11 +61,12 @@ return {
         map('n', ';hz', '<cmd>Gitsigns undo_stage_hunk<cr>')
         map('n', ';hR', '<cmd>Gitsigns reset_buffer<cr>')
         map('n', ';hp', '<cmd>Gitsigns preview_hunk<cr>')
+        map('n', ';hx', '<cmd>Gitsigns toggle_deleted<cr>', { desc = 'Gitsigns toggle show deletiion' })
         map('n', ';gb', '<cmd>lua require"gitsigns".blame_line{full=true}<cr>')
         map('n', ';gB', '<cmd>Gitsigns toggle_current_line_blame<cr>')
         map('n', ';gd', '<cmd>Gitsigns diffthis<cr>')
         map('n', ';gD', '<cmd>lua require"gitsigns".diffthis("~")<cr>')
-        map('n', ';gx', '<cmd>Gitsigns toggle_deleted<cr>')
+        map('n', ';gx', '<cmd>Gitsigns toggle_deleted<cr>', { desc = 'Gitsigns toggle show deletiion' })
 
         -- Text object
         map('o', 'ih', '<cmd><C-U>Gitsigns select_hunk<cr>')
