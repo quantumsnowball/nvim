5d7c60b44a1f3e9940efd99e05f37504862b8e7a
Quantum Snowball <quantum.snowball@gmail.com>
2026-09-09 18:09:58 +0800

    auto complete label don't be too wide

--- lua/plugins/blink.cmp.lua
+++ lua/plugins/blink.cmp.lua
@@ -79,6 +79,7 @@ return {
                         label = {
                             text = function(ctx) return require('colorful-menu').blink_components_text(ctx) end,
                             highlight = function(ctx) return require('colorful-menu').blink_components_highlight(ctx) end,
+                            width = { max = math.floor(vim.o.columns * 0.75) },
                         },
                     },
                 },
