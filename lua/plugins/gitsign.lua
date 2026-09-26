afaf14a4f5c85819fae20a15ad64aacd497aa322
Quantum Snowball <quantum.snowball@gmail.com>
2026-09-26 17:11:36 +0800

    ,n ,p ,, navigator between hunk in buffer

--- lua/plugins/gitsign.lua
+++ lua/plugins/gitsign.lua
@@ -50,6 +50,9 @@ return {
         -- Navigation
         map('n', ']c', '<cmd>Gitsigns next_hunk<cr>')
         map('n', '[c', '<cmd>Gitsigns prev_hunk<cr>')
+        map('n', '<leader>n', '<cmd>Gitsigns next_hunk<cr>')
+        map('n', '<leader>p', '<cmd>Gitsigns prev_hunk<cr>')
+        map('n', '<leader><leader>', '<cmd>Gitsigns next_hunk<cr>')
 
         -- Actions
         map('n', '<leader>s', '<cmd>Gitsigns stage_hunk<cr>')
